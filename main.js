e838d136274c1d165ae39f3a421e68f80920d5dd
Avani-4-4-10 <92733721+Avani-4-4-10@users.noreply.github.com>
2022-06-16 18:59:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-song1=""
-song2=""
+song=""
 leftWristX=0
 rightWristX=0
 leftWristY=0
 rightWristY=0
+scoreLeftWristy=0
+scoreRightWrist= 0 
+
 
 function preload(){
-    song1=loadSound("music.mp3")
-    song2=loadSound("music2.mp3")
+    song=loadSound("music.mp3")
 }
 
 function setup(){
@@ -22,17 +23,62 @@ posenet.on("pose" , gotPoses)
 
 function draw(){
     image(video , 0 , 0 , 500 , 400 )
+
+    fill("red")
+    stroke("blue")
+
+    if(scoreRightWrist > 0.2){
+        circle(rightWristX , rightWristY , 20)
+
+        if(rightWristY > 0 && rightWristY <= 100){
+            song.rate(0.5)
+            document.getElementById("speed").innerHTML="Speed= 0.5x "
+        }
+        else if(rightWristY > 100 && rightWristY <= 200){
+            song.rate(1)
+            document.getElementById("speed").innerHTML = "Speed= 1x"
+        }
+        else if(rightWristY > 200 && rightWristY <= 300){
+            song.rate(1.5)
+            document.getElementById("speed").innerHTML = "Speed =1.5x"
+        }
+        else if(rightWristY > 300 && rightWristY <= 400){
+            song.rate(2)
+            document.getElementById("speed").innerHTML= "Speed =2x"
+
+        }
+        else if(rightWristY > 400 && rightWristY <= 500){
+            song.rate(2.5)
+            document.getElementById("speed").innerHTML= "Speed= 2.5x "
+        }
+
+    }
+
+   if(scoreLeftWristy > 0.2){
+       circle(leftWristX , leftWristY , 20)
+
+       leftWristY_number= Number(leftWristY)
+       remove_decimals = Math.floor(leftWristY_number)
+       volume = remove_decimals/500
+       song.setVolume(volume)
+
+       document.getElementById("volume").innerHTML="volume = " + volume
+   }
 }
 
 function play(){
-    song1.play()
+    song.play()
+    song.setVolume(1)
+    song.rate(1)
 }
 
 function modelLoaded(){
     console.log("Posenet is initialized!")
+
 }
 
 function gotPoses(results){
+
     if(results.length > 0){
 
         console.log(results)
@@ -41,8 +87,13 @@ function gotPoses(results){
         rightWristX= results[0].pose.rightWrist.x
         leftWristY= results[0].pose.leftWrist.y
         rightWristY= results[0].pose.rightWrist.y
+        scoreLeftWristy=results[0].pose.leftWrist.confidence
+        scoreRightWrist=results[0].pose.rightWrist.confidence
 
-        console.log(leftWristX , rightWristX)
-        console.log(leftWristY , rightWristY)
+        console.log(leftWristX , leftWristY)
+        console.log(rightWristX , rightWristY)
+        console.log(scoreLeftWristy)
+        console.log(scoreRightWrist)
+
     }
 }
